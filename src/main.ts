0f917bd4f5397a72576bec23c506dcc634289fb1
nyh <nsi3358@naver.com>
2024-07-19 18:01:58 +0900

    PR-TEST

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  //서버 시작지점
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
 }
