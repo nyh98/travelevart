1001218248ed740a0ed8692ebd98d43f51562c0e
chansik0504 <chansik0504@gmail.com>
2024-08-14 23:37:18 +0900

    MuJungDan Test2

--- src/healthcheck/healthcheck.controller.ts
+++ src/healthcheck/healthcheck.controller.ts
@@ -1,25 +1,10 @@
 import { Controller, Get } from '@nestjs/common';
-import * as os from 'os';
 
 @Controller('healthcheck')
 export class HealthCheckController {
   @Get()
   check(): string {
-    const serverIp = this.getServerIp();
-    console.log(`Health check called on server IP: ${serverIp}`);
+    console.log("여기다@@@@@@@@@@@@@@@@@@@");
     return 'OK';
-  }
-
-  private getServerIp(): string {
-    const networkInterfaces = os.networkInterfaces();
-    for (const interfaceName of Object.keys(networkInterfaces)) {
-      const networkInterface = networkInterfaces[interfaceName];
-      for (const iface of networkInterface) {
-        if (iface.family === 'IPv4' && !iface.internal) {
-          return iface.address; // 서버의 IPv4 주소를 반환
-        }
-      }
-    }
-    return 'IP not found';
   }
 }
